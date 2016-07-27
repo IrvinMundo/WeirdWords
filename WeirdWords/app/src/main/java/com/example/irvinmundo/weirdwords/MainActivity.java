d21410579b81ec6dfccbf5bf981f083f3bef7e6f
Emiliano Cervantes <ovman96@hotmail.com>
2016-07-27 10:46:37 -0500

    Nueva función
    La función para hallar el idioma

--- WeirdWords/app/src/main/java/com/example/irvinmundo/weirdwords/MainActivity.java
+++ WeirdWords/app/src/main/java/com/example/irvinmundo/weirdwords/MainActivity.java
@@ -3,19 +3,26 @@ package com.example.irvinmundo.weirdwords;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
+    }
+
+    public String onDetectLanguage(){
+        String yourYesResponse = "Hello";
+
         if (Locale.getDefault().getLanguage().equals("en")) {
-             yourYesResponse = "That is great " + usersName + "!";
-         }
-         else if (Locale.getDefault().getLanguage().equals("fr")) {
-             yourYesResponse = "C\'est bon " + usersName + "!";
-         }
-         */
-        //agredando un comentario para ver que funcione Git
+            yourYesResponse = "Name of the app";
+        } else if (Locale.getDefault().getLanguage().equals("es")) {
+            yourYesResponse = "Nombre de la app";
+        }
+
+        return yourYesResponse;
+    }
+
 }
