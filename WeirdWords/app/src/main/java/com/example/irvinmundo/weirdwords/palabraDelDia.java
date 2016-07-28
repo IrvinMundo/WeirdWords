a5fbc9e266057343cf429d1fc343f116ff2f8210
Alfredo Puente <alfredopv95@gmail.com>
2016-07-28 10:59:07 -0500

    Intento de respuesta al bug
    Se intentó sin éxito resolver el problema del bug de cuando se presiona
    un botón se cierra la app

--- WeirdWords/app/src/main/java/com/example/irvinmundo/weirdwords/palabraDelDia.java
+++ WeirdWords/app/src/main/java/com/example/irvinmundo/weirdwords/palabraDelDia.java
@@ -1,9 +1,17 @@
 package com.example.irvinmundo.weirdwords;
 
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 /**
  * Created by ovman on 26/07/2016.
  */
-public class palabraDelDia {
+public class palabraDelDia extends AppCompatActivity{
 
     //aquí trabajaremos con las fusion tables
     String palabraDia;
@@ -11,4 +19,15 @@ public class palabraDelDia {
     public palabraDelDia(String palabraDia){
         palabraDia = "tacos";
     }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.palabra_del_dia_layout);
+    }
+
+    //private void setContentView(int palabra_del_dia_layout) {
+    //}
 }
+
+
+
