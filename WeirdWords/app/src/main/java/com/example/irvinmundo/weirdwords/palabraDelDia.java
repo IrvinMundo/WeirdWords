33df04681b7d7abebec69f1d3e62b5b723e223b7
Emiliano Cervantes <ovman96@hotmail.com>
2016-08-16 18:38:18 -0500

    New button
    Simple changes

--- WeirdWords/app/src/main/java/com/example/irvinmundo/weirdwords/palabraDelDia.java
+++ WeirdWords/app/src/main/java/com/example/irvinmundo/weirdwords/palabraDelDia.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 /**
@@ -16,9 +17,8 @@ public class palabraDelDia extends AppCompatActivity{
     //aquí trabajaremos con las fusion tables
     String palabraDia;
 
-    /*public palabraDelDia(String palabraDia){
-        palabraDia = "tacos";
-    }*/
+    //button to send word of the day to Diccionary
+    private ImageButton imageButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
