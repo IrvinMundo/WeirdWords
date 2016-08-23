9515dceb71ecbc46bce312b27e5a66c07eb5444e
Alfredo Puente <alfredopv95@gmail.com>
2016-08-23 18:35:42 -0500

    Conexión Fusion Tables

---
+++ WeirdWords/app/src/main/java/com/example/irvinmundo/weirdwords/ConexionFusionTable.java
@@ -0,0 +1,34 @@
+package com.example.irvinmundo.weirdwords;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.fusiontables.Fusiontables;
+import com.google.api.services.fusiontables.Fusiontables.Query.Sql;
+import com.google.api.services.fusiontables.Fusiontables.Table.Delete;
+import com.google.api.services.fusiontables.FusiontablesScopes;
+import com.google.api.services.fusiontables.model.Column;
+import com.google.api.services.fusiontables.model.Table;
+import com.google.api.services.fusiontables.model.TableList;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Alfredo on 23/08/2016.
+ */
+public class ConexionFusionTable {
+}
